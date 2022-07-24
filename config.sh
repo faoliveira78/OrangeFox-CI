94dfce037afa32e36e1edc667af884a44daa4d07
Fernando Oliveira <fernandoaju78@gmail.com>
2022-07-24 14:17:54 -0300

    cheeseburger build

--- config.sh
+++ config.sh
@@ -2,9 +2,9 @@
 
 # Device
 export FOX_BRANCH="fox_12.1"
-export DT_LINK="https://github.com/faoliveira78/android_device_oneplus_dumpling -b android-12.1"
+export DT_LINK="https://github.com/faoliveira78/android_device_oneplus_cheeseburger -b android-12.1"
 
-export DEVICE="dumpling"
+export DEVICE="cheeseburger"
 export OEM="oneplus"
 export DT_PATH="device/$OEM/$DEVICE"
 
